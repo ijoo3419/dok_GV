636fa966d4b4813d913ca0e30d9c755f6141a882
KOSUNGHEE <KOSUNGHEE@DESKTOP-0MDGHLC>
2018-09-21 14:54:54 +0900

    성희 로그인 완성!!!! 로그아웃도 됨^^

--- src/main/java/com/kh/dok/member/controller/MemberController.java
+++ src/main/java/com/kh/dok/member/controller/MemberController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+import com.kh.dok.member.model.exception.LoginException;
 import com.kh.dok.member.model.service.MemberService;
 import com.kh.dok.member.model.vo.Member;
 
@@ -56,9 +58,27 @@ public class MemberController {
       return "member/signup";
    } 
    
-/*   @RequestMapping("login.me")
+   @RequestMapping("loginCheck.me")
    public String loginCheck(Member m, Model model){
-      model.addAttribute("loginUser", ms.loginMember(m));
-   }*/
+	   
+	   System.out.println("MemberController : " + m);
+      try {
+		model.addAttribute("loginUser", ms.loginMember(m));
+		
+		return "main/main";
+	} catch (LoginException e) {
+		model.addAttribute("msg", e.getMessage());
+		
+		return "common/errorPage";
+	}
+      
+   }
+   
+   @RequestMapping("logout.me")
+   public String logout(SessionStatus status){
+	   status.setComplete();
+	   
+	   return "main/main";
+   }
 
 }
