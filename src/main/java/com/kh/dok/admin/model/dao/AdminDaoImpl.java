9ca015d4abbb1bba64bf31d1a595895af1beef87
seepieex <39686489+seepieex@users.noreply.github.com>
2018-09-27 09:06:30 +0900

    09.27 09:00_인소진

--- src/main/java/com/kh/dok/admin/model/dao/AdminDaoImpl.java
+++ src/main/java/com/kh/dok/admin/model/dao/AdminDaoImpl.java
@@ -2,18 +2,72 @@ package com.kh.dok.admin.model.dao;
 
 import java.util.ArrayList;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.dok.common.PageInfo;
 import com.kh.dok.member.model.vo.Member;
 
 @Repository
 public class AdminDaoImpl implements AdminDao{
 
+	//전체 회원 조회 메소드
 	@Override
-	public ArrayList<Member> searchAll(SqlSessionTemplate sqlSession) {
+	public ArrayList<Member> searchAll(SqlSessionTemplate sqlSession,PageInfo pi) {
+		ArrayList<Member> mlist = null;
 		
-		return (ArrayList)sqlSession.selectList("Admin.searchAll");
+		int offset = (pi.getCurrentPage()-1)* pi.getLimit();
+		
+		RowBounds rowBound = new RowBounds(offset, pi.getLimit());
+		
+		
+		mlist = (ArrayList)sqlSession.selectList("Admin.searchAll",null, rowBound);
+		
+		return mlist;
+	}
+
+	//일반 회원 조회 메소드
+	@Override
+	public ArrayList<Member> searchBu(SqlSessionTemplate sqlSession,PageInfo pi) {
+		
+		int offset = (pi.getCurrentPage()-1) * pi.getLimit();
+		
+		RowBounds rowBound = new RowBounds(offset,pi.getLimit());
+		
+		return (ArrayList)sqlSession.selectList("Admin.searchBu",null,rowBound);
+	}
+
+	//판매자 회원 조회 메소드
+	@Override
+	public ArrayList<Member> searchSe(SqlSessionTemplate sqlSession,PageInfo pi) {
+		
+		int offset = (pi.getCurrentPage()-1) * pi.getLimit();
+		
+		RowBounds rowBound = new RowBounds(offset,pi.getLimit());
+		
+		return (ArrayList)sqlSession.selectList("Admin.searchSe",null,rowBound);
+	}
+
+	//전체 회원 조회  카운트 메소드
+	@Override
+	public int countAll(SqlSessionTemplate sqlSession) {
+		
+		return sqlSession.selectOne("Admin.countAll");
+	}
+
+	//일반 회원  조회 카운트 메소드
+	@Override
+	public int countBu(SqlSessionTemplate sqlSession) {
+		
+		return sqlSession.selectOne("Admin.countBu");
+	}
+
+	//판매자 회원 조회 카운트 메소드
+	@Override
+	public int countSe(SqlSessionTemplate sqlSession) {
+		
+		return sqlSession.selectOne("Admin.countSe");
 	}
 
 	
