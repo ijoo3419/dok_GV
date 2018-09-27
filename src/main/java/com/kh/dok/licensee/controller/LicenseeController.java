ffebd4bfcda1e5bfc1c80c2cf15d5b35240b7140
qwertyJeong <qwertyJeong@DESKTOP-J4GF886>
2018-09-27 15:08:57 +0900

    영화관 등록 하는중~ 정태_0927_2

--- src/main/java/com/kh/dok/licensee/controller/LicenseeController.java
+++ src/main/java/com/kh/dok/licensee/controller/LicenseeController.java
@@ -1,10 +1,28 @@
 package com.kh.dok.licensee.controller;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kh.dok.cinema.model.vo.Cinema;
+import com.kh.dok.common.CommonUtils;
+import com.kh.dok.licensee.model.service.LicenseeService;
 
 @Controller
+@SessionAttributes("loginUser")
 public class LicenseeController {
+	@Autowired
+	private LicenseeService ls;
+
 	@RequestMapping("licensee.li")
 	public String showLicenseeMainView(){
 		return "licensee/mainLicenseePage";
@@ -44,5 +62,43 @@ public class LicenseeController {
 	@RequestMapping("theater.li")
 	public String showTheaterView(){
 		return "licensee/theaterManagePage";
+		
 	}
+	
+	//정태 영화관 등록 메소드(+파일 첨부)
+	@RequestMapping("theaterInsert.li")
+	public String insertTheater(Model model, Cinema cm, 
+								HttpServletRequest request, 
+								@RequestParam(name="photo", required=false)MultipartFile photo){
+		
+		System.out.println("첫번째 photo = " + photo);
+		System.out.println("두번째 cm = " + cm);
+		
+		String root = request.getSession().getServletContext().getRealPath("resources");
+		String filePath = root + "\\uploadFiles";
+		
+		String originFileName = photo.getOriginalFilename();
+		String ext = originFileName.substring(originFileName.lastIndexOf("."));
+		String changeName = CommonUtils.getRandomString();
+		
+		
+		try {
+			photo.transferTo(new File(filePath + "\\" + changeName + ext));
+	
+			ls.insertTheater(cm);
+			System.out.println(cm);
+			System.out.println("성공하면 여기로");
+			return "licensee/theaterManagePage";
+		
+		} catch (Exception e){
+			new File(filePath + "\\" + changeName + ext).delete();
+			
+			System.out.println("실패하면 여기로");
+			model.addAttribute("msg", "영화관 추가 실패!");
+			
+			return "common/errorPage";
+			
+		}
+	}
+		
 }
