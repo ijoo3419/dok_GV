7892c91f8bee13ebe3f62e44a992dbca1a6bd35d
seepieex <39686489+seepieex@users.noreply.github.com>
2018-10-20 15:41:13 +0900

    10.17_12:00 인소진

--- src/main/java/com/kh/dok/admin/controller/AdminController.java
+++ src/main/java/com/kh/dok/admin/controller/AdminController.java
@@ -313,6 +313,17 @@ public class AdminController {
 		return "admin/adminPage";
 	}
 
+	//사용자 맞춤 영화 추천
+	@RequestMapping("recomend.ad")
+	public String recomend(Model model,String mid){
+		String tab = "tab-8";
 	
+		model.addAttribute("tab", tab);
+		
+		ArrayList<String> genreList = as.selectGenre(mid);
+		
+		model.addAttribute("genreList", genreList);
+		return "admin/adminPage";
+	}
 }
 
